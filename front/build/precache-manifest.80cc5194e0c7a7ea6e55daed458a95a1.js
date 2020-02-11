612c01ce0e8aafab9a8371bcf8f6fcdb163f121b
VSPaul <vascupaul_vsp@yahoo.com>
2020-02-11 17:24:45 +0200

    da

---
+++ front/build/precache-manifest.80cc5194e0c7a7ea6e55daed458a95a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542472f0deca8829ef851844f9128585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba02279ffe81c58fdb11",
+    "url": "/static/css/main.aeb76980.chunk.css"
+  },
+  {
+    "revision": "76fa420b2e19aa69b66c",
+    "url": "/static/js/2.2cf42e74.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.2cf42e74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba02279ffe81c58fdb11",
+    "url": "/static/js/main.0667e22a.chunk.js"
+  },
+  {
+    "revision": "b45713355acf61d40e30",
+    "url": "/static/js/runtime-main.ee8a1ed5.js"
+  },
+  {
+    "revision": "82e8df0ae9464ed819e8a611ff63b0d2",
+    "url": "/static/media/mystery-avatar.82e8df0a.jpg"
+  }
+]);
